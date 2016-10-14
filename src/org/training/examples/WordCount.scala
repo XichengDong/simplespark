84321502c1b11a9d658b25455b02e0f1124641c8
Xicheng Dong <xicheng.dong@hulu.com>
2016-10-14 19:19:45 +0800

    Allow to customize input & output path.

--- src/org/training/examples/WordCount.scala
+++ src/org/training/examples/WordCount.scala
@@ -5,11 +5,18 @@ import org.apache.spark._
 object WordCount {
   def main(args: Array[String]) {
     var masterUrl = "local[1]"
-    if (args.length > 0) {
+    var inputPath = "/Users/xicheng.dong/training-examples/ml-1m/README"
+    var outputPath = "/tmp/output"
+
+    if (args.length == 1) {
       masterUrl = args(0)
+    } else if (args.length == 3) {
+      masterUrl = args(0)
+      inputPath = args(1)
+      outputPath = args(2)
     }
-    val inputPath = "/Users/xicheng.dong/training-examples/ml-1m/README"
-    val outputPath = "/tmp/output"
+
+    println(s"masterUrl:${masterUrl}, inputPath: ${inputPath}, outputPath: ${outputPath}")
 
     val sparkConf = new SparkConf().setMaster(masterUrl).setAppName("WordCount")
     val sc = new SparkContext(sparkConf)
@@ -17,10 +24,6 @@ object WordCount {
     val rowRdd = sc.textFile(inputPath)
     val resultRdd = rowRdd.flatMap(line => line.split("\\s+"))
         .map(word => (word, 1)).reduceByKey(_ + _)
-
-    resultRdd.count()
-
-    resultRdd.first()
 
     resultRdd.saveAsTextFile(outputPath)
   }
